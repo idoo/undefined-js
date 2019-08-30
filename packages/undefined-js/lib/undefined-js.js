29fea71a067010205e8f94561bee17854bc76a99
Ivan Verevkin <ivan.p.verevkin@gmail.com>
2019-08-30 14:26:05 +0200

    feat: added slow implementation (beta)

--- packages/undefined-js/lib/undefined-js.js
+++ packages/undefined-js/lib/undefined-js.js
@@ -5,3 +5,18 @@ module.exports = undefinedJs;
 function undefinedJs(value) {
   return typeof value === 'undefined';
 }
+
+function hasSomeValue() {
+  // TODO: add implementation for that cases
+  // false
+  // 0
+  // ""
+  // NaN
+  // null
+  // undefined
+}
+
+function slowCheckForUndefined(value) {
+  // TODO: use async/await here
+  return setTimeout(function(){ undefinedJs(value) }, 1000);
+}
